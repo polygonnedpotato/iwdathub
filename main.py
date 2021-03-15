bd855c0cc95b3378547e0657f683d6c23d8ba580
elburg the polygonned potato <berge61319@loswego.k12.or.us>
2021-03-15 21:11:19 +0000

    new files

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from flask import Flask
+import status as dathubstatus
+import assetget as astg
+import parsecommand as parse
 app = Flask('app')
 
 @app.route('/')
 def root_home():
   return 'err:inv_syt'
 @app.route('/statuspage')
-def get_status()
-
-
+def get_status():
+  return 'err:not_implement'
+@app.route('/getasset/<string>')
+def returnasset(string):
+  return 'err:not_implement'
 app.run(host='0.0.0.0', port=8080)
